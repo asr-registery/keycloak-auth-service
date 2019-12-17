54093f8b65725d1ae87ccf8474987f58b34b7f29
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-17 12:14:08 +0430

    init of test cases

---
+++ src/test/java/af/asr/auth/test/AuthTestBootApplication.java
@@ -0,0 +1,21 @@
+package af.asr.auth.test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * The Class AuthTestBootApplication.
+ */
+@SpringBootApplication
+public class AuthTestBootApplication {
+	
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(AuthTestBootApplication.class, args);
+	}
+
+}
